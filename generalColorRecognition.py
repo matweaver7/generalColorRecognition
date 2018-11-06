0bf2e9a7b9384198ced659be92c243f97294eae9
Mat Weaver <matweaver7@gmail.com>
2018-11-05 22:41:54 -0700

    beginning of understanding of cv2

--- generalColorRecognition.py
+++ generalColorRecognition.py
@@ -1,3 +1,16 @@
 import cv2
 import matplotlib as plt
+import numpy as np
 
+try:
+    img = cv2.imread('forest.jpeg')
+except:
+    print("this failed")
+
+wind = cv2.namedWindow("cool")
+cv2.imshow("cool", img)
+k = cv2.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
+px = img[100,100]
+print(px)
